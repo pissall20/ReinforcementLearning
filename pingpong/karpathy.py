0edf6c7caa865c89d738c9a523789ababa2b4c03
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2019-08-27 17:06:47 +0530

    Reformatted files

--- pingpong/karpathy.py
+++ pingpong/karpathy.py
@@ -15,13 +15,15 @@ render = False
 # model initialization
 D = 80 * 80  # input dimensionality: 80x80 grid
 if resume:
-    model = pickle.load(open('save.p', 'rb'))
+    model = pickle.load(open("save.p", "rb"))
 else:
     model = dict()
-    model['W1'] = np.random.randn(H, D) / np.sqrt(D)  # "Xavier" initialization
-    model['W2'] = np.random.randn(H) / np.sqrt(H)
+    model["W1"] = np.random.randn(H, D) / np.sqrt(D)  # "Xavier" initialization
+    model["W2"] = np.random.randn(H) / np.sqrt(H)
 
-grad_buffer = {k: np.zeros_like(v) for k, v in model.items()}  # update buffers that add up gradients over a batch
+grad_buffer = {
+    k: np.zeros_like(v) for k, v in model.items()
+}  # update buffers that add up gradients over a batch
 rmsprop_cache = {k: np.zeros_like(v) for k, v in model.items()}  # rmsprop memory
 
 
@@ -45,16 +47,18 @@ def discount_rewards(r):
     running_add = 0
     for t in reversed(range(0, r.size)):
         if r[t] != 0:
-            running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
+            running_add = (
+                0
+            )  # reset the sum, since this was a game boundary (pong specific!)
         running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
     return discounted_r
 
 
 def policy_forward(x):
-    h = np.dot(model['W1'], x)
+    h = np.dot(model["W1"], x)
     h[h < 0] = 0  # ReLU nonlinearity
-    logp = np.dot(model['W2'], h)
+    logp = np.dot(model["W2"], h)
     p = sigmoid(logp)
     return p, h  # return probability of taking action 2, and hidden state
 
@@ -62,10 +66,10 @@ def policy_forward(x):
 def policy_backward(eph, epdlogp):
     """ backward pass. (eph is array of intermediate hidden states) """
     dW2 = np.dot(eph.T, epdlogp).ravel()
-    dh = np.outer(epdlogp, model['W2'])
+    dh = np.outer(epdlogp, model["W2"])
     dh[eph <= 0] = 0  # backprop relu
     dW1 = np.dot(dh.T, epx)
-    return {'W1': dW1, 'W2': dW2}
+    return {"W1": dW1, "W2": dW2}
 
 
 env = gym.make("Pong-v0")
@@ -99,7 +103,9 @@ while True:
     observation, reward, done, info = env.step(action)
     reward_sum += reward
 
-    drs.append(reward)  # record reward (has to be done after we call step() to get reward for previous action)
+    drs.append(
+        reward
+    )  # record reward (has to be done after we call step() to get reward for previous action)
 
     if done:  # an episode finished
         episode_number += 1
@@ -117,26 +123,40 @@ while True:
         discounted_epr -= np.mean(discounted_epr)
         discounted_epr /= np.std(discounted_epr)
 
-        epdlogp *= discounted_epr  # modulate the gradient with advantage (PG magic happens right here.)
+        epdlogp *= (
+            discounted_epr
+        )  # modulate the gradient with advantage (PG magic happens right here.)
         grad = policy_backward(eph, epdlogp)
-        for k in model: grad_buffer[k] += grad[k]  # accumulate grad over batch
+        for k in model:
+            grad_buffer[k] += grad[k]  # accumulate grad over batch
 
         # perform rmsprop parameter update every batch_size episodes
         if episode_number % batch_size == 0:
             for k, v in model.items():
                 g = grad_buffer[k]  # gradient
-                rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g ** 2
+                rmsprop_cache[k] = (
+                    decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g ** 2
+                )
                 model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
                 grad_buffer[k] = np.zeros_like(v)  # reset batch gradient buffer
 
         # boring book-keeping
-        running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-        print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+        running_reward = (
+            reward_sum
+            if running_reward is None
+            else running_reward * 0.99 + reward_sum * 0.01
+        )
+        print(
+            "resetting env. episode reward total was %f. running mean: %f"
+            % (reward_sum, running_reward)
+        )
         if episode_number % 100 == 0:
-            pickle.dump(model, open('save.p', 'wb'))
+            pickle.dump(model, open("save.p", "wb"))
         reward_sum = 0
         observation = env.reset()  # reset env
         prev_x = None
 
-    if reward != 0 and reward is not None:  # Pong has either +1 or -1 reward exactly when game ends.
-        print('ep %d: game finished, reward: %f' % (episode_number, reward))
+    if (
+        reward != 0 and reward is not None
+    ):  # Pong has either +1 or -1 reward exactly when game ends.
+        print("ep %d: game finished, reward: %f" % (episode_number, reward))
--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -56,13 +56,17 @@ def remove_background(image):
     return image
 
 
-def preprocess_observations(input_observation, prev_processed_observation, input_dimensions):
+def preprocess_observations(
+    input_observation, prev_processed_observation, input_dimensions
+):
     """ convert the 210x160x3 uint8 frame into a 6400 float vector """
     processed_observation = input_observation[35:195]  # crop
     processed_observation = downsample(processed_observation)
     processed_observation = remove_color(processed_observation)
     processed_observation = remove_background(processed_observation)
-    processed_observation[processed_observation != 0] = 1  # everything else (paddles, ball) just set to 1
+    processed_observation[
+        processed_observation != 0
+    ] = 1  # everything else (paddles, ball) just set to 1
     # Convert from 80 x 80 matrix to 1600 x 1 matrix
     processed_observation = processed_observation.astype(np.float).ravel()
 
@@ -87,9 +91,9 @@ def relu(vector):
 
 def apply_neural_nets(observation_matrix, weights):
     """ Based on the observation_matrix and weights, compute the new hidden layer values and the new output layer values"""
-    hidden_layer_values = np.dot(weights['1'], observation_matrix)
+    hidden_layer_values = np.dot(weights["1"], observation_matrix)
     hidden_layer_values = relu(hidden_layer_values)
-    output_layer_values = np.dot(hidden_layer_values, weights['2'])
+    output_layer_values = np.dot(hidden_layer_values, weights["2"])
     output_layer_values = sigmoid(output_layer_values)
     return hidden_layer_values, output_layer_values
 
@@ -108,13 +112,10 @@ def compute_gradient(gradient_log_p, hidden_layer_values, observation_values, we
     """ See here: http://neuralnetworksanddeeplearning.com/chap2.html"""
     delta_L = gradient_log_p
     dC_dw2 = np.dot(hidden_layer_values.T, delta_L).ravel()
-    delta_l2 = np.outer(delta_L, weights['2'])
+    delta_l2 = np.outer(delta_L, weights["2"])
     delta_l2 = relu(delta_l2)
     dC_dw1 = np.dot(delta_l2.T, observation_values)
-    return {
-        '1': dC_dw1,
-        '2': dC_dw2
-    }
+    return {"1": dC_dw1, "2": dC_dw2}
 
 
 def update_weights(weights, expectation_g_squared, g_dict, decay_rate, learning_rate):
@@ -122,9 +123,15 @@ def update_weights(weights, expectation_g_squared, g_dict, decay_rate, learning_
     epsilon = 1e-5
     for layer_name in weights.keys():
         g = g_dict[layer_name]
-        expectation_g_squared[layer_name] = decay_rate * expectation_g_squared[layer_name] + (1 - decay_rate) * g ** 2
-        weights[layer_name] += (learning_rate * g) / (np.sqrt(expectation_g_squared[layer_name] + epsilon))
-        g_dict[layer_name] = np.zeros_like(weights[layer_name])  # reset batch gradient buffer
+        expectation_g_squared[layer_name] = (
+            decay_rate * expectation_g_squared[layer_name] + (1 - decay_rate) * g ** 2
+        )
+        weights[layer_name] += (learning_rate * g) / (
+            np.sqrt(expectation_g_squared[layer_name] + epsilon)
+        )
+        g_dict[layer_name] = np.zeros_like(
+            weights[layer_name]
+        )  # reset batch gradient buffer
 
 
 def discount_rewards(rewards, gamma):
@@ -134,7 +141,9 @@ def discount_rewards(rewards, gamma):
     running_add = 0
     for t in reversed(range(0, rewards.size)):
         if rewards[t] != 0:
-            running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
+            running_add = (
+                0
+            )  # reset the sum, since this was a game boundary (pong specific!)
         running_add = running_add * gamma + rewards[t]
         discounted_rewards[t] = running_add
     return discounted_rewards
@@ -168,11 +177,13 @@ def main():
     prev_processed_observations = None
 
     if resume:
-        weights = pickle.load(open('save.p', 'rb'))
+        weights = pickle.load(open("save.p", "rb"))
     else:
         weights = {
-            '1': np.random.randn(num_hidden_layer_neurons, input_dimensions) / np.sqrt(input_dimensions),
-            '2': np.random.randn(num_hidden_layer_neurons) / np.sqrt(num_hidden_layer_neurons)
+            "1": np.random.randn(num_hidden_layer_neurons, input_dimensions)
+            / np.sqrt(input_dimensions),
+            "2": np.random.randn(num_hidden_layer_neurons)
+            / np.sqrt(num_hidden_layer_neurons),
         }
 
     # To be used with rmsprop algorithm (http://sebastianruder.com/optimizing-gradient-descent/index.html#rmsprop)
@@ -182,14 +193,21 @@ def main():
         expectation_g_squared[layer_name] = np.zeros_like(weights[layer_name])
         g_dict[layer_name] = np.zeros_like(weights[layer_name])
 
-    episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = [], [], [], []
+    episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = (
+        [],
+        [],
+        [],
+        [],
+    )
 
     while True:
         env.render()
-        processed_observations, prev_processed_observations = preprocess_observations(observation,
-                                                                                      prev_processed_observations,
-                                                                                      input_dimensions)
-        hidden_layer_values, up_probability = apply_neural_nets(processed_observations, weights)
+        processed_observations, prev_processed_observations = preprocess_observations(
+            observation, prev_processed_observations, input_dimensions
+        )
+        hidden_layer_values, up_probability = apply_neural_nets(
+            processed_observations, weights
+        )
 
         episode_observations.append(processed_observations)
         episode_hidden_layer_values.append(hidden_layer_values)
@@ -217,13 +235,15 @@ def main():
             episode_rewards = np.vstack(episode_rewards)
 
             # Tweak the gradient of the log_ps based on the discounted rewards
-            episode_gradient_log_ps_discounted = discount_with_rewards(episode_gradient_log_ps, episode_rewards, gamma)
+            episode_gradient_log_ps_discounted = discount_with_rewards(
+                episode_gradient_log_ps, episode_rewards, gamma
+            )
 
             gradient = compute_gradient(
                 episode_gradient_log_ps_discounted,
                 episode_hidden_layer_values,
                 episode_observations,
-                weights
+                weights,
             )
 
             # Sum the gradient for use when we hit the batch size
@@ -231,16 +251,30 @@ def main():
                 g_dict[layer_name] += gradient[layer_name]
 
             if episode_number % batch_size == 0:
-                update_weights(weights, expectation_g_squared, g_dict, decay_rate, learning_rate)
+                update_weights(
+                    weights, expectation_g_squared, g_dict, decay_rate, learning_rate
+                )
 
-            episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = [], [], [], []  # reset values
+            episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = (
+                [],
+                [],
+                [],
+                [],
+            )  # reset values
             observation = env.reset()  # reset env
-            running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-            print('episode number: %d' % episode_number)
-            print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+            running_reward = (
+                reward_sum
+                if running_reward is None
+                else running_reward * 0.99 + reward_sum * 0.01
+            )
+            print("episode number: %d" % episode_number)
+            print(
+                "resetting env. episode reward total was %f. running mean: %f"
+                % (reward_sum, running_reward)
+            )
 
             if episode_number % 100 == 0:
-                pickle.dump(weights, open('save.p', 'wb'))
+                pickle.dump(weights, open("save.p", "wb"))
 
             reward_sum = 0
             prev_processed_observations = None
